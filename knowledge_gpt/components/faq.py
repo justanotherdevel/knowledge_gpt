17bfc8670b91671fd0e27d0bc9f2eef560cf8c10
Shashwat Yashaswi <shashwatyashaswi@gmail.com>
2024-12-25 18:20:54 +0530

    Feat: Green Credit AI Branch

--- knowledge_gpt/components/faq.py
+++ knowledge_gpt/components/faq.py
@@ -6,17 +6,17 @@ def faq():
     st.markdown(
         """
 # FAQ
-## How does Trinity AI work?
+## How does Green Credit AI work?
 When you upload a document, it will be divided into smaller chunks 
 and stored in a special type of database called a vector index 
 that allows for semantic search and retrieval.
 
-When you ask a question, Trinity AI will search through the
+When you ask a question, Green Credit AI will search through the
 document chunks and find the most relevant ones using the vector index.
 Then, it will use GPT4 to generate a final answer.
 
 ## Is my data safe?
-Yes, your data is safe. Trinity AI does not store your documents or
+Yes, your data is safe. Green Credit AI does not store your documents or
 questions. All uploaded data is deleted after you close the browser tab.
 
 ## Why does it take so long to index my document?
@@ -31,15 +31,15 @@ the chunk number on that page. For DOCS and TXT documents,
 the first number is set to 1 and the second number is the chunk number.
 
 ## Are the answers 100% accurate?
-No, the answers are not 100% accurate. Trinity AI uses GPT-4 to generate
+No, the answers are not 100% accurate. Green Credit AI uses GPT-4 to generate
 answers. GPT-4 is a powerful language model, but it sometimes makes mistakes 
-and is prone to hallucinations. Also, Trinity AI uses semantic search
+and is prone to hallucinations. Also, Green Credit AI uses semantic search
 to find the most relevant chunks and does not see the entire document,
 which means that it may not be able to find all the relevant information and
 may not be able to answer all questions (especially summary-type questions
 or questions that require a lot of context from the document).
 
-But for most use cases, Trinity AI is very accurate and can answer
+But for most use cases, Green Credit AI is very accurate and can answer
 most questions. Always check with the sources to make sure that the answers
 are correct.
 """
--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -29,7 +29,7 @@ def sidebar():
         # st.markdown("---")
         st.markdown("# About")
         st.markdown(
-            "📖Trinity AI allows you to ask questions about your "
+            "📖Green Credit AI allows you to ask questions about your "
             "documents and get accurate answers with instant citations. "
         )
 
